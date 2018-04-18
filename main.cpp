e5f0323222a7ec8b4812ed8e4e445cf750188fe3
Camilo Higuita <milo.h@aol.com>
2018-04-17 22:38:32 -0500

    remove qquickstyle for now

--- main.cpp
+++ main.cpp
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include <QQuickStyle>
 #include <QIcon>
 #include <QCommandLineParser>
 #include <QFileInfo>
