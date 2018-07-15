b24e17322acc88ebd89159d7901ed3ed46ada159
Camilo Higuita <milo.h@aol.com>
2018-07-15 16:45:40 -0500

    push updates

--- main.cpp
+++ main.cpp
@@ -38,7 +38,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #include "mauikit/src/mauikit.h"
-#include "tagging.h"
 
 QStringList getFolderImages(const QString &path)
 {
