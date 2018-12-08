a9a1511538abdcb626656c4029a8d2455fd3adb3
Bruno Miguel Queiros <bqueiros@gmail.com>
2018-12-08 02:16:18 +0000

    add qml-module-qmltermwidget

--- travis-build.sh
+++ travis-build.sh
@@ -10,7 +10,9 @@ wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
 ### Install Dependencies
 apt-get --yes update
 apt-get --yes dist-upgrade
-apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs qt5-default qtdeclarative5-dev qtquickcontrols2-5-dev qtwebengine5-dev cmake debhelper extra-cmake-modules libkf5config-dev libkf5coreaddons-dev libkf5i18n-dev libkf5kio-dev libkf5notifications-dev libkf5service-dev libqt5svg5-dev qtbase5-dev  qml-module-org-kde-kirigami2 qml-module-qmltermwidget qml-module-qtquick-controls2 qml-module-qtwebengine
+apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs qt5-default qtdeclarative5-dev qtquickcontrols2-5-dev qtwebengine5-dev cmake debhelper extra-cmake-modules libkf5config-dev libkf5coreaddons-dev libkf5i18n-dev libkf5kio-dev libkf5notifications-dev libkf5service-dev libqt5svg5-dev qtbase5-dev  qml-module-org-kde-kirigami2 qml-module-qtquick-controls2 qml-module-qtwebengine
+wget https://raw.githubusercontent.com/lnxslck/home/master/nitrux/qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb -O qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
+dpkg -i qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
 wget http://repo.nxos.org/development/pool/main/m/mauikit/libmauikit_0%2Bgit20181114-1_amd64.deb
 dpkg -i libmauikit_0+git20181114-1_amd64.deb
 wget http://repo.nxos.org/development/pool/main/m/mauikit/qml-module-org-kde-mauikit_0%2Bgit20181114-1_amd64.deb
