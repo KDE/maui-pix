b8a803265c3181f116d2b96d767538b0c7c96049
lnxslck <bqueiros@gmail.com>
2019-03-08 02:14:31 +0000

    Added our testing repository

--- travis-build.sh
+++ travis-build.sh
@@ -7,18 +7,15 @@ apt-get --yes install wget gnupg2
 echo 'deb http://archive.neon.kde.org/dev/stable/ bionic main' | tee /etc/apt/sources.list.d/neon-stable.list
 wget -qO - 'http://archive.neon.kde.org/public.key' | apt-key add -
 
+### Add NXOS Repository
+echo 'deb http://repo.nxos.org/testing nxos main' | tee /etc/apt/sources.list.d/nxos-testing.list
+wget -qO - 'http://repo.nxos.org/public.key' | apt-key add -
+
 ### Install Dependencies
 apt-get --yes update
 apt-get --yes dist-upgrade
-apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs qt5-default qtdeclarative5-dev qtquickcontrols2-5-dev qtwebengine5-dev cmake debhelper extra-cmake-modules libkf5config-dev libkf5coreaddons-dev libkf5i18n-dev libkf5kio-dev libkf5notifications-dev libkf5service-dev libqt5svg5-dev qtbase5-dev  qml-module-org-kde-kirigami2 qml-module-qtquick-controls2 qml-module-qtwebengine
-wget https://raw.githubusercontent.com/lnxslck/home/master/nitrux/qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb -O qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
-dpkg -i qml-module-qmltermwidget_0.1%2Bgit20180903-1_amd64.deb
-wget http://repo.nxos.org/development/pool/main/m/mauikit/libmauikit_0%2Bgit20181114-1_amd64.deb
-dpkg -i libmauikit_0+git20181114-1_amd64.deb
-wget http://repo.nxos.org/development/pool/main/m/mauikit/qml-module-org-kde-mauikit_0%2Bgit20181114-1_amd64.deb
-dpkg -i qml-module-org-kde-mauikit_0+git20181114-1_amd64.deb
-wget http://repo.nxos.org/development/pool/main/m/mauikit/mauikit-dev_0%2Bgit20181114-1_amd64.deb 
-dpkg -i mauikit-dev_0+git20181114-1_amd64.deb
+apt-get --yes install devscripts lintian build-essential automake autotools-dev equivs qt5-default qtdeclarative5-dev qtquickcontrols2-5-dev qtwebengine5-dev cmake debhelper extra-cmake-modules libkf5config-dev libkf5coreaddons-dev libkf5i18n-dev libkf5kio-dev libkf5notifications-dev libkf5service-dev libqt5svg5-dev qtbase5-dev  qml-module-org-kde-kirigami2 qml-module-qtquick-controls2 qml-module-qtwebengine qml-module-qmltermwidget libmauikit qml-module-org-kde-mauikit mauikit-dev
+
 mk-build-deps -i -t "apt-get --yes" -r
 
 ### Build Deb
