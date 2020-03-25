5a36a054c479ab5f4cf1ac07d38d4c5da9f7123a
Camilo Higuita <milo.h@aol.com>
2020-03-25 17:37:49 -0500

    fixes for latest mauikit

--- src/db/dbactions.cpp
+++ src/db/dbactions.cpp
@@ -28,132 +28,132 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 DBActions::DBActions(QObject *parent) : DB(parent)
 {
-    qDebug() << "Getting collectionDB info from: " << PIX::CollectionDBPath;
-    qDebug()<< "Starting DBActions";
+	qDebug() << "Getting collectionDB info from: " << PIX::CollectionDBPath;
+	qDebug()<< "Starting DBActions";
 }
 
 DBActions *DBActions::instance = nullptr;
 DBActions *DBActions::getInstance()
 {
-    if(!instance)
-    {
-        qDebug() << "getInstance(): First DBActions instance\n";
-        instance = new DBActions();
-        return instance;
-    } else
-    {
-        qDebug()<< "getInstance(): previous DBActions instance\n";
-        return instance;
-    }
+	if(!instance)
+	{
+		qDebug() << "getInstance(): First DBActions instance\n";
+		instance = new DBActions();
+		return instance;
+	} else
+	{
+		qDebug()<< "getInstance(): previous DBActions instance\n";
+		return instance;
+	}
 }
 
 bool DBActions::execQuery(const QString &queryTxt)
 {
-    auto query = this->getQuery(queryTxt);
-    return query.exec();
+	auto query = this->getQuery(queryTxt);
+	return query.exec();
 }
 
 bool DBActions::insertPic(const FMH::MODEL &img)
 {
-    QVariantMap sourceMap {{FMH::MODEL_NAME[FMH::MODEL_KEY::URL], img[FMH::MODEL_KEY::SOURCE]}};
-    this->insert(PIX::TABLEMAP[PIX::TABLE::SOURCES], sourceMap);
-    return this->insert(PIX::TABLEMAP[PIX::TABLE::IMAGES], FMH::toMap(img));
+	QVariantMap sourceMap {{FMH::MODEL_NAME[FMH::MODEL_KEY::URL], img[FMH::MODEL_KEY::SOURCE]}};
+	this->insert(PIX::TABLEMAP[PIX::TABLE::SOURCES], sourceMap);
+	return this->insert(PIX::TABLEMAP[PIX::TABLE::IMAGES], FMH::toMap(img));
 }
 
 bool DBActions::addPic(const QUrl &url)
 {
-    if(!this->checkExistance(PIX::TABLEMAP[PIX::TABLE::IMAGES], FMH::MODEL_NAME[FMH::MODEL_KEY::URL], url.toString()))
-    {
-        const QFileInfo info(url.toLocalFile());
-        FMH::MODEL picMap =
-        {
-            {FMH::MODEL_KEY::URL, url.toString()},
-            {FMH::MODEL_KEY::TITLE,  info.baseName()},
-            {FMH::MODEL_KEY::RATE, "0"},
-            {FMH::MODEL_KEY::COLOR, ""},
-            {FMH::MODEL_KEY::SIZE, QString::number(info.size())},
-            {FMH::MODEL_KEY::SOURCE, FMH::fileDir(url)},
-            {FMH::MODEL_KEY::DATE, info.birthTime().toString(Qt::TextDate)},
-            {FMH::MODEL_KEY::ADDDATE, QDateTime::currentDateTime().toString(Qt::TextDate)},
-            {FMH::MODEL_KEY::FORMAT, info.suffix()}
-        };
+	if(!this->checkExistance(PIX::TABLEMAP[PIX::TABLE::IMAGES], FMH::MODEL_NAME[FMH::MODEL_KEY::URL], url.toString()))
+	{
+		const QFileInfo info(url.toLocalFile());
+		FMH::MODEL picMap =
+		{
+			{FMH::MODEL_KEY::URL, url.toString()},
+			{FMH::MODEL_KEY::TITLE,  info.baseName()},
+			{FMH::MODEL_KEY::RATE, "0"},
+			{FMH::MODEL_KEY::COLOR, ""},
+			{FMH::MODEL_KEY::SIZE, QString::number(info.size())},
+			{FMH::MODEL_KEY::SOURCE, FMH::fileDir(url)},
+			{FMH::MODEL_KEY::DATE, info.birthTime().toString(Qt::TextDate)},
+			{FMH::MODEL_KEY::ADDDATE, QDateTime::currentDateTime().toString(Qt::TextDate)},
+			{FMH::MODEL_KEY::FORMAT, info.suffix()}
+		};
 
-        return this->insertPic(picMap);
-    }
+		return this->insertPic(picMap);
+	}
 
-    return false;
+	return false;
 }
 
 bool DBActions::removePic(const QString &url)
 {
-    auto queryTxt = QString("DELETE FROM images WHERE url =  \"%1\"").arg(url);
-    auto query = this->getQuery(queryTxt);
-    if(query.exec())
-    {
-        queryTxt = QString("DELETE FROM images_notes WHERE url =  \"%1\"").arg(url);
-        this->getQuery(queryTxt).exec();
+	auto queryTxt = QString("DELETE FROM images WHERE url =  \"%1\"").arg(url);
+	auto query = this->getQuery(queryTxt);
+	if(query.exec())
+	{
+		queryTxt = QString("DELETE FROM images_notes WHERE url =  \"%1\"").arg(url);
+		this->getQuery(queryTxt).exec();
 
-        return true;
-    }
-    return false;
+		return true;
+	}
+	return false;
 }
 
 bool DBActions::deletePic(const QUrl &url)
 {
-    if(FMStatic::removeFile(url))
-        return this->removePic(url.toString());
+	if(FMStatic::removeFiles({url}))
+		return this->removePic(url.toString());
 
-    return false;
+	return false;
 }
 
 QVariantList DBActions::searchFor(const QStringList &queries, const QString &queryTxt)
 {
-    QVariantList res;
-    for(auto query : queries)
-        res <<  this->get(PIX::getQuery("searchFor_").arg(query));
+	QVariantList res;
+	for(auto query : queries)
+		res <<  this->get(PIX::getQuery("searchFor_").arg(query));
 
-    return res;
+	return res;
 }
 
 FMH::MODEL_LIST DBActions::getFolders(const QString &query)
 {
-    const auto data = this->getDBData(query);
-    return std::accumulate(data.begin(), data.end(), FMH::MODEL_LIST(), [](FMH::MODEL_LIST &res, const FMH::MODEL &item){
-        res << FMH::getFileInfoModel(item[FMH::MODEL_KEY::URL]);
-        return res;
-    });
+	const auto data = this->getDBData(query);
+	return std::accumulate(data.begin(), data.end(), FMH::MODEL_LIST(), [](FMH::MODEL_LIST &res, const FMH::MODEL &item){
+		res << FMH::getFileInfoModel(item[FMH::MODEL_KEY::URL]);
+		return res;
+	});
 }
 
 FMH::MODEL_LIST DBActions::getDBData(const QString &queryTxt, std::function<bool(FMH::MODEL &item)> modifier)
 {
-    FMH::MODEL_LIST mapList;
+	FMH::MODEL_LIST mapList;
 
-    auto query = this->getQuery(queryTxt);
+	auto query = this->getQuery(queryTxt);
 
-    if(query.exec())
-    {
-        while(query.next())
-        {
-            FMH::MODEL data;
-            for(const auto &key : FMH::MODEL_NAME.keys())
-                if(query.record().indexOf(FMH::MODEL_NAME[key]) > -1)
-                    data.insert(key, query.value(FMH::MODEL_NAME[key]).toString());
+	if(query.exec())
+	{
+		while(query.next())
+		{
+			FMH::MODEL data;
+			for(const auto &key : FMH::MODEL_NAME.keys())
+				if(query.record().indexOf(FMH::MODEL_NAME[key]) > -1)
+					data.insert(key, query.value(FMH::MODEL_NAME[key]).toString());
 
-            if(modifier)
-            {
-                if(!modifier(data))
-                    continue;
-            }
+			if(modifier)
+			{
+				if(!modifier(data))
+					continue;
+			}
 
-            mapList << data;
-        }
+			mapList << data;
+		}
 
-    }else qDebug()<< query.lastError()<< query.lastQuery();
+	}else qDebug()<< query.lastError()<< query.lastQuery();
 
-    return mapList;
+	return mapList;
 }
 
 QVariantList DBActions::get(const QString &queryTxt)
 {
-    return this->getList(queryTxt);
+	return this->getList(queryTxt);
 }
