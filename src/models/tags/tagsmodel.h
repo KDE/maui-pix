11bbada47ffd2563713d6542342f4630f3e111e4
Camilo Higuita <chiguitar@unal.edu.co>
2020-10-27 15:37:02 -0500

    add tagmodle sources and update ignoreme file

---
+++ src/models/tags/tagsmodel.h
@@ -0,0 +1,30 @@
+#ifndef TAGSMODEL_H
+#define TAGSMODEL_H
+
+#ifdef STATIC_MAUIKIT
+#include "fmh.h"
+#include "mauilist.h"
+#else
+#include <MauiKit/fmh.h>
+#include <MauiKit/mauilist.h>
+#endif
+
+class TagsModel : public MauiList
+{
+    Q_OBJECT
+
+public:
+    explicit TagsModel(QObject *parent = nullptr);
+    FMH::MODEL_LIST items() const override;
+    void componentComplete() override;
+
+private:
+    FMH::MODEL_LIST list;
+    void setList();
+
+    FMH::MODEL_LIST tags();
+
+    void packPreviewImages(FMH::MODEL &tag);
+};
+
+#endif // TAGSMODEL_H
